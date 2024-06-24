015dd9cb634b8a12abc6742a418dcf8ea186f0d9
Christian <chris.346@hotmail.com>
2024-06-24 13:55:42 -0400

    Enter first commit

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+let initialCards = [
+  {
+    name: "El Pico Duarte",
+    link: "https://unsplash.com/photos/forest-covered-with-fogs-W7oPDwcqccQ",
+  },
+  {
+    name: "Mount Rushmore",
+    link: "https://unsplash.com/photos/a-group-of-presidents-carved-into-the-side-of-a-mountain-JqR0rtygBfM",
+  },
+  {
+    name: "Yellowstone Park",
+    link: "https://unsplash.com/photos/green-trees-under-blue-sky-and-white-clouds-during-daytime-eprFKRjtoE0",
+  },
+  {
+    name: "The Everglades",
+    link: "https://unsplash.com/photos/leafless-trees-between-river-rTkEjSss93U",
+  },
+  {
+    name: "Death Valley",
+    link: "https://unsplash.com/photos/brown-concrete-road-during-daytime-2TQwrtZnl08",
+  },
+  {
+    name: "Bahia de las Aguilas",
+    link: "https://unsplash.com/photos/brown-rock-formation-beside-blue-sea-under-blue-sky-and-white-clouds-during-daytime-EEwfeFr6J44",
+  },
+];
