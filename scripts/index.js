89020276b93196dde5110e5da8b6d73fb3d78c89
Christian <chris.346@hotmail.com>
2024-07-12 18:02:46 -0400

    Enter first commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,4 @@
-const initialCards = [
-  {
+const initialCards = [{
     name: "El Pico Duarte",
     link: "https://images.unsplash.com/photo-1538430352266-de6bcba9a06b?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
@@ -54,19 +53,22 @@ const cardListEl = document.querySelector(".gallery");
 
 //function to close the modal
 function closePopUp(modal) {
-  /*profileEditModal*/modal.classList.remove("modal_opened");
+  modal.classList.remove("modal_opened");
+  /*profileEditModal*/
 }
-function openPopUp(modal){
-modal.classList.add("modal_opened");
+
+function openPopUp(modal) {
+  modal.classList.add("modal_opened");
 }
-function renderCard(cardElement , container){
+
+function renderCard(cardElement, container) {
   container.prepend(cardElement);
 }
 
 //work to be done here!!!
 //get a clone of card template and add the new source img and text
 function getCardView(cardData) {
-  const cardElement = cardTemplate.cloneNode(true);//true brings all the child
+  const cardElement = cardTemplate.cloneNode(true); //true brings all the child
 
   const cardImageEl = cardElement.querySelector(".card__image"); // to open picrure modal by clicking this element
   const cardTitleEl = cardElement.querySelector(".card__text");
@@ -79,33 +81,33 @@ function getCardView(cardData) {
   cardTitleEl.textContent = cardData.name; //set text name of image to the obj
 
   //addEventlistner for image modal
-    cardImageEl.addEventListener('click',()=>{
-      pictureModalImage.setAttribute('src', cardData.link) //go ot the image element and change it to the image source that is  in line 69
-      pictureModalAltText.textContent = cardData.name// switch the alternate text of the modal
-      openPopUp(pictureModal) //open popup function with the (previewimagemodal)
-    })
-    pictureModalCloseBtn.addEventListener('click',()=>{
-      closePopUp(pictureModal)
-    })
+  cardImageEl.addEventListener('click', () => {
+    pictureModalImage.setAttribute('src', cardData.link) //go ot the image element and change it to the image source that is  in line 69
+    pictureModalAltText.textContent = cardData.name // switch the alternate text of the modal
+    openPopUp(pictureModal) //open popup function with the (previewimagemodal)
+  })
+  pictureModalCloseBtn.addEventListener('click', () => {
+    closePopUp(pictureModal)
+  })
 
-      //then finally smooth opening of the modals fade in and fade out
+  //then finally smooth opening of the modals fade in and fade out
 
 
-   //add event listner for like
-   cardLikeBtn.addEventListener('click',()=>{
+  //add event listner for like
+  cardLikeBtn.addEventListener('click', () => {
     cardLikeBtn.classList.toggle('card__heart_active') // add active class to cardlikebutton
-   })
+  })
 
-   //find trash icon
-   const trashIcon = cardElement.querySelector('.card__trashcan-btn')
-   //add event listner for delete
-   trashIcon.addEventListener('click',()=>{
-     cardElement.remove(); //go to card element and call remove in it cardEl.remove()
-   })
+  //find trash icon
+  const trashIcon = cardElement.querySelector('.card__trashcan-btn')
+  //add event listner for delete
+  trashIcon.addEventListener('click', () => {
+    cardElement.remove(); //go to card element and call remove in it cardEl.remove()
+  })
 
   return cardElement;
-   //cardListEl.prepend(cardElement);
-   // the two below could be used directly on the foreach loop
+  //cardListEl.prepend(cardElement);
+  // the two below could be used directly on the foreach loop
   //const cardElement = renderCardElement(cardData);
   //return cardElement;
 }
@@ -114,14 +116,14 @@ function getCardView(cardData) {
 function handleAddEditButton() {
   profileTitleInput.value = profileTitle.textContent;
   profileDescriptionInput.value = profileDescription.textContent;
- // profileEditModal.classList.add("modal_opened");
- openPopUp(profileEditModal);
+  // profileEditModal.classList.add("modal_opened");
+  openPopUp(profileEditModal);
 }
 
 //handler to set the text inside the edit
 function handleAddButton() {
   //profileTitleInput.value = profileTitle.textContent;
- // profileDescriptionInput.value = profileDescription.textContent;
+  // profileDescriptionInput.value = profileDescription.textContent;
   // above are not needed for that modal
   openPopUp(cardAddModal);
 }
@@ -131,41 +133,46 @@ function handleProfileEditSubmit(event) {
   event.preventDefault();
   profileTitle.textContent = profileTitleInput.value;
   profileDescription.textContent = profileDescriptionInput.value;
-  closePopUp();
+  closePopUp(profileEditModal);
 }
-function handleCardAddSubmit(e){
-e.preventDefault();
-//grab values of the input form then set them whats will be inputted
-const name = e.target.title.value // title is the name attribute
-const link = e.target.link.value
 
-//call render card function and pass an object with name and link
-const cardView = getCardView({
-  name,
-  link
-})
-/*renderCard({name:title,link:link})*/
+function handleCardAddSubmit(e) {
+  e.preventDefault();
+  //grab values of the input form then set them whats will be inputted
+  const name = e.target.title.value // title is the name attribute
+  const link = e.target.link.value
 
-  renderCard(cardView ,cardListEl)
+  //call render card function and pass an object with name and link
+  const cardView = getCardView({
+    name,
+    link
+  })
+  /*renderCard({name:title,link:link})*/
+
+  renderCard(cardView, cardListEl)
   closePopUp(cardAddModal)
 }
 
 
 profileEditForm.addEventListener("submit", handleProfileEditSubmit);
-profileEditBtn.addEventListener("click",handleAddEditButton); /*()=>{the body of toggle edit button could go here} */
-profileModalCloseBtn.addEventListener("click", () => {closePopUp(profileEditModal); /* you could delete the arrow function and just use the name of closePopup as reference*/});
-cardAddBtn.addEventListener('click', handleAddButton );
-cardAddCloseButton.addEventListener('click',()=>{closePopUp(cardAddModal)})
+profileEditBtn.addEventListener("click", handleAddEditButton); /*()=>{the body of toggle edit button could go here} */
+profileModalCloseBtn.addEventListener("click", () => {
+  closePopUp(profileEditModal); /* you could delete the arrow function and just use the name of closePopup as reference*/
+});
+cardAddBtn.addEventListener('click', handleAddButton);
+cardAddCloseButton.addEventListener('click', () => {
+  closePopUp(cardAddModal)
+})
 cardAddForm.addEventListener('submit', handleCardAddSubmit)
 
 
 //places each card into the list in the DOM
 initialCards.forEach((cardData) => {
- // const cardElement = renderCardElement(cardData);
+  // const cardElement = renderCardElement(cardData);
   //cardListEl.append(cardElement);//apends to the end of cardlist
 
-const cardView = getCardView(cardData)
-  renderCard(cardView , cardListEl)//needs to pass card data variable which is the object iterations
+  const cardView = getCardView(cardData)
+  renderCard(cardView, cardListEl) //needs to pass card data variable which is the object iterations
 });
 
 /*
@@ -196,6 +203,3 @@ initialCards.forEach((cardData) => {
 
 //on trashcan user .remove() in the element in order to remove it
 // the like button needs an addeventlistener click
-
-
-
