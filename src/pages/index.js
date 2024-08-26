bb165b38379614a1daf92b85771f646cf4c54f72
Christian <chris.346@hotmail.com>
2024-08-26 13:16:22 -0400

    added webpack

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import "../pages/index.css";
 import FormValidator from "../scripts/FormValidator.js";
 import Card from "../scripts/Card.js";
 //import {closePopUp,openPopUp,escapeModalHandler} from"../utils/utils.js"
